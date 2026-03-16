9296647df7e357f849ab7bbb7159bf8c9e3b1e0b
esp0xdeadbeef <esp0xdeadbeef@gmail.com>
2026-03-16 01:46:23 +0100

    commented old routers, and added 8.8.8.8 (still works)

--- run-in-vm.sh
+++ run-in-vm.sh
@@ -4,7 +4,7 @@ docker-clab-frr-plus-tooling/build.sh
 
 containerlab deploy -t fabric.clab.yml -d --reconfigure
 
-for c in $(docker ps --format '{{.Names}}' | grep clab-fabric)
+for c in $(docker ps --format '{{.Names}}' | grep clab-fabric | sort )
 do
   echo "=================================================="
   echo "NODE: $c"
@@ -36,20 +36,20 @@ do
   docker exec "$c" ip neigh
   echo
 
-  echo "[ ip route get 1.1.1.1 ]"
-  docker exec "$c" ip route get 1.1.1.1
+  echo "[ ip route get 8.8.8.8 ]"
+  docker exec "$c" ip route get 8.8.8.8
   echo
 
-  echo "[ traceroute -> s-router-access (10.10.0.0) ]"
-  docker exec "$c" traceroute -I -n -w 1 -q 1 -m 8 10.10.0.0 || true
-  echo
+  #echo "[ traceroute -> s-router-access (10.10.0.0) ]"
+  #docker exec "$c" traceroute -I -n -w 1 -q 1 -m 8 10.10.0.0 || true
+  #echo
 
-  echo "[ traceroute -> s-router-core-isp-a (10.10.0.2) ]"
-  docker exec "$c" traceroute -I -n -w 1 -q 1 -m 5 10.10.0.2 || true
-  echo
+  #echo "[ traceroute -> s-router-core-isp-a (10.10.0.2) ]"
+  #docker exec "$c" traceroute -I -n -w 1 -q 1 -m 5 10.10.0.2 || true
+  #echo
 
-  echo "[ traceroute -> internet (1.1.1.1) ]"
-  docker exec "$c" traceroute -I -n -w 1 -q 1 -m 8 1.1.1.1 || true
+  echo "[ traceroute -> internet (8.8.8.8) ]"
+  docker exec "$c" traceroute -I -n -w 1 -q 1 -m 8 8.8.8.8 || true
   echo
 
   echo " [ FIREWALL - nft list ruleset]"
