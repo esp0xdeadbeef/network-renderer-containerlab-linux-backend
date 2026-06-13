8ff00e53c46143d3af9a283b8b081e6210d0dc84
deadbeef <deadbeef@localhost>
2026-06-13 21:49:34 +0200

    URS L97: emit VLAN2 management from CPM deploymentHosts data passed via rendererInput.managementVlan

--- host-module.nix
+++ host-module.nix
@@ -293,9 +293,16 @@ in
     '';
   };
 
+  # ----- VLAN 2 management (from CPM inventory, per URS) -----
   # ----- VLAN 4 upstream internet (persistent across reboots) -----
-  # VLAN 4 is the emulated internet uplink: eth0.4 → br-uplink0 → container WAN
   systemd.network.netdevs = {
+    "10-eth0.2" = lib.mkIf (containerlabLinuxRendererInput ? managementVlan && containerlabLinuxRendererInput.managementVlan != null) {
+      netdevConfig = { Kind = "vlan"; Name = "eth0.2"; };
+      vlanConfig = { Id = 2; };
+    };
+    "20-vlan2" = lib.mkIf (containerlabLinuxRendererInput ? managementVlan && containerlabLinuxRendererInput.managementVlan != null) {
+      netdevConfig = { Kind = "bridge"; Name = "vlan2"; };
+    };
     "10-eth0.4" = {
       netdevConfig = {
         Kind = "vlan";
@@ -314,6 +321,35 @@ in
   };
 
   systemd.network.networks = {
+    "10-eth0" = lib.mkIf (containerlabLinuxRendererInput ? managementVlan && containerlabLinuxRendererInput.managementVlan != null) {
+      matchConfig.Name = "eth0";
+      linkConfig.ActivationPolicy = "always-up";
+      networkConfig = {
+        ConfigureWithoutCarrier = true;
+        DHCP = "no";
+        LinkLocalAddressing = "no";
+        VLAN = [ "eth0.2" "eth0.4" ];
+      };
+    };
+    "20-eth0.2" = lib.mkIf (containerlabLinuxRendererInput ? managementVlan && containerlabLinuxRendererInput.managementVlan != null) {
+      matchConfig.Name = "eth0.2";
+      linkConfig.ActivationPolicy = "always-up";
+      networkConfig = {
+        ConfigureWithoutCarrier = true;
+        DHCP = "no";
+        LinkLocalAddressing = "no";
+        Bridge = "vlan2";
+      };
+    };
+    "30-vlan2" = lib.mkIf (containerlabLinuxRendererInput ? managementVlan && containerlabLinuxRendererInput.managementVlan != null) {
+      matchConfig.Name = "vlan2";
+      linkConfig.ActivationPolicy = "always-up";
+      networkConfig = {
+        DHCP = "ipv4";
+        LinkLocalAddressing = "no";
+        IPv6AcceptRA = false;
+      };
+    };
     "10-eth0.4" = {
       matchConfig.Name = "eth0.4";
       linkConfig.ActivationPolicy = "always-up";
